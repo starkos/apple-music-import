918949ab2427821cc86ff20c3623e3ec3400e09a
starkos <starkos@industriousone.com>
2020-01-11 13:52:11 -0500

    Bootstrap a macOS application project

---
+++ AppleImport/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace AppleImport
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
